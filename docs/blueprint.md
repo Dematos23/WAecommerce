28b9fb522ff5b575d3a0515dac3a6cce9c278f2d
Diego Matos <dematos23@gmail.com>
2025-08-02 07:31:54 -0500

    preparing to delete firebase project

--- docs/blueprint.md
+++ docs/blueprint.md
@@ -1,4 +1,4 @@
-# **App Name**: TiendaExpress
+# **App Name**: Kima
 
 ## Core Features:
 
--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,6 +2,7 @@
 
 import * as admin from 'firebase-admin';
 import type { Firestore } from 'firebase-admin/firestore';
+import serviceAccount from '../../serviceAccountKey.json'; 
 
 console.log('--- Verificando variables de entorno ---');
 console.log('PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
@@ -20,11 +21,7 @@ if (hasAdminConfig && !admin.apps.length) {
   try {
     console.log('--- Inicializando Firebase Admin SDK ---');
     admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: "kima-oicw0",
-        clientEmail: "firebase-adminsdk-fbsvc@kima-oicw0.iam.gserviceaccount.com",
-        privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCXeA3x5sAiG5oU\nDceETSpmxwRGXoHakEM47bXSi+vhb+qi21IuhX0bKNZSsvxx6ac+NVWlCwo3q0dY\nBaoMAZx7MJPUKEtHyn+MQeF7j76e3El/i0SinZkQut3jvkCn/I4PoKsX3lZ/si7n\n8rzBCCdFrobhvnTMEQzoGqz+8VkYZaC3gmd9/BR1Rh4+takvgxzsQCQgFDiH/FK7\nU0MbBrIyEFqA04bNSWTx5t02DcPqiChqZ0kp2ndgDUHIQxxZZec9RxOhaq0OebV3\njGlnd14NIpSruyJki62bG+ZPcUKm1GZXSDcUmgDeMzvLph8nJ4esSZAKlrSb4HCw\nDAFsIcXVAgMBAAECggEADTGtP0rbJi1y1rfbjxP8s063Odz+ITiLToBwY+qBzdWm\nofCKYFiK7Qx53OLP+ClDAvl44CNNzMsb8BgwN5MFWzrASc0Hv7VtNl2/wzhigaqN\nMXHDdQnW3Tz12qKGNcvbYROdC7t3m/B8wwIjPIpWB20cPTpOqL78eZRnSN7jpKsF\nTaIwrhUvH0eTN2pp3//LJ637qL5ZjWHaf8CFeMVhvFHAWEXub9NTJqH7hFA+0y7B\nM5OWfowgQ2JsdDmuAT/qmtV2eJd/ytiZOIG9RpbexvTsgMqql8Vsvuf4FoDmRil0\nwliA+d77kW46rLcDrbGtRZZFbquwFp8AzRiwxbQJSQKBgQDHVuS27bel8aGIQEvi\nCw/XMKgayWja0mREkuAiaQBBE0Ga69URO99qPM15yHR/UpLg8Tta4zcqOJvc80ZJ\nqkRgSv2tHWA9Ms2MTwOq66suaBZjA/QIB0+3gNq4AvLOd08OVlgBFRRo17n3D8cc\n4akEbE3QoIvMoC+mHYqfy7W6rQKBgQDChdM63BTMfDwibsM6P4oUX2DdJXnKIWEg\nnhh3dy+YveEMYBPZ8yN2zWX5aomfYeJzPKR0nu3ib/rkH+ddxOrNX3/48xjNofbB\nPXpTUMCn96mkHqwrIJJLoGfE1udgA7iO+roXx33n6vLsjT91747njYnG+ROa+/06\n/YZPDhWEyQKBgQC9n9xMY9BKjJQz763/5rP36bJQYP5dAr0dOnb6ZNY4EWBcRjFW\nhpUeE58HVXf00pEkaHYlHHYStO0X/1gCpXUMAOpWLtapANXlvhhoFG2v5JY/NLiE\nlqfvkST+3NcrLvXGVr66sNnMbkLaUmkAAW5t5bUUXFUAn/UT2ZBQOFG0tQKBgQCa\nSWUCAX+ptTxJH0tPzwfpIsGEhm30CcetBFhpoSoqXyi6FRaJv1S4rS+dxYfE1/Gx\n7z6NlWUfPixkdn2fnUmTfZTWU5KvvzAlk2qL3LgCQejW2NE8H8iJnX/Q25eHS0w+\nc9XYeI1/vpXRVDMqisjxFg1auUJ4ojf3axD1cTlWKQKBgCd0Efmv8hPkM7vbjqqy\ni1n2doaec+UH85Km1Zu8EGAuhk0hzaf+EjFy121i2RJ2xGCrMKrHX9YlgAZvgjoI\nMaTojsGp5Kf6vtT+ilMJIrJ2OwHrcNeHtnP8Z48MN7KAT1QRdf0c9fLjdSjzLHfF\nrztXkbK5KoQ1rdaNBctnFEAD\n-----END PRIVATE KEY-----\n".replace(/\\n/g, '\n'),
-      }),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
     console.log('Firebase Admin SDK inicializado correctamente');
     adminDb = admin.firestore();
